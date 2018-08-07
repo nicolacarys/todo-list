cb22dea89814afbfc4fd4bc5f86e95f7704b8ba3
Nicola Richardson <nicolacarys@users.noreply.github.com>
2018-08-07 14:32:17 +0100

    Added extra filter

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -2,14 +2,12 @@ import React from "react";
 
 export default ({ onChange, value, addItem }) => (
 	<div>
-        <input 
-        	onChange={ onChange } 
-        	value={ value } 
-        	type="text" 
-        	placeholder="What do you need to do?"
-        />
-        <button 
-        	onClick={ addItem }
-        >Add Todo</button>
-    </div>
+    <input 
+    	onChange={ onChange } 
+    	value={ value } 
+    	type="text" 
+    	placeholder="What do you need to do?"
+    />
+    <button onClick={ addItem }>Add Todo</button>
+  </div>
 );
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default ({ onClick }) => (
 	<ul>
-        <li onClick={ () => onClick(0) }>All</li>
-        <li onClick={ () => onClick(1) }>Completed</li>
-        <li onClick={ () => onClick(2) }>To Do</li>
-        <li onClick={ () => onClick(3) }>Deleted</li>
-    </ul>
+    <li onClick={ () => onClick(0) }>All</li>
+    <li onClick={ () => onClick(1) }>Completed</li>
+    <li onClick={ () => onClick(2) }>To Do</li>
+    <li onClick={ () => onClick(3) }>Deleted</li>
+  </ul>
 );
